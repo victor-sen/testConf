24f17c7b8b7ce181e5b5c1d4012a94a403525519
ProvarCyril <77401653+ProvarCyril@users.noreply.github.com>
2021-12-07 15:56:14 -0600

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 const SF_BASE_URI = `https://e2edemo.my.salesforce.com`
-const SF_BEARER_TOKEN = `00D5f000003vd1P!ARUAQAxzVe_VUJhWxRTbMrmHo9L1dwKhaoa2aUz2xtU3mb1k6R49boXEIF90fLUYasd1fkRO.WRDoWAaftUE1rTSXlFhOX.m`
+const SF_BEARER_TOKEN = `00D5f000003vd1P!ARUAQAxzVe_VUJhWxRTbMrmHo9L1dwKhaoa2aUz2xtU3mb1k6R49boXEIF90fLUYasd1fkRO.WRDoWAaftUE1rTSXlFhOX.`
 
 window.addEventListener('load', (event) => {
   var speaker_data = null;
